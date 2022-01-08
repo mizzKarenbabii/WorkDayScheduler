cb606100f7309de28b5c493eb3f6b9ba87cd629a
mizzKarenbabii <mizzkarenbabii@gmail.com>
2022-01-08 11:32:00 -0500

    added save and local storage

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-// this code displays the current date
-const d = new Date();
-document.getElementById("currentDay").innerHTML = d;
+$(document).ready(function() {
 
 
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
 
-//single event i.e. alarm, time in milliseconds
-var timeout = setTimeout(function(){yourFunction()},10000);
-//repeated events, gap in milliseconds
-var interval = setInterval(function(){yourFunction()},1000);
+
+var schedule =  function () {
+    var actualTime = moment().hours();
+   $(".time-block").each(function(){
+       var rowTime = parseInt($(this).attr("id").split("-")[1]);
+       
+        if (rowTime < actualTime) {
+            $(this).addClass("past")
+        } 
+        else if (rowTime === actualTime){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+        }
+        else {
+            $(this).removeClass("present")
+            $(this).removeClass("past")
+            $(this).addClass("future")
+        }
+
+   })
+
+}
+schedule();
+
+$(".saveBtn").on("click", function(){
+    //localStorage.setItem("key", "value");
+    //localStarage.getItem("key")
+    var timeKey = $(this).parent().attr("id");
+    var timeValue = $(this).siblings(".description").val();
+localStorage.setItem(timeKey, timeValue)
+})
+// saves it to local storage!!!! does not disappear when page is refreshed
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13.description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
+ 
+
+});
